d8f0c944c3aac1df082efd2000e55e59d383608f
JG <32463353+juliaguida@users.noreply.github.com>
2019-06-08 14:47:48 +0100

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,134 @@
+document.write("You live you learn, if knowledge is the power learning is your power.");
+
+/*function addSomething (num,str) {
+
+//all elements go inside the parentheses.
+
+///var add = num + str;
+
+//alert (add);
+
+//}
+ 
+//addSomething (44 , "Pink starts");
+
+
+
+// A statement has end with a ; sign as the statement above.
+
+// var day; //Declare var without assigning value.
+// var assignValue = 1144; //Declare var and assign a value.
+// var years = 35; //Js is a loosely laguage as you don't really have to explicit declare a variable you can simpley say years = 35; however is not recommended.
+
+ //Java in ethe other hand is a strongly language it means you must explicit declare 
+//int var years = 35,
+
+// var someText = "Peace";//Must use single '' or double" quotation marks for strings.
+// var love = "True"; //Boolean- True or False.
+
+//var can only start with letters.
+//var can only start with lower case or _ space is replaced by _
+
+//Function is the action the behaviour. where we work with our variable.
+// alert (assignValue + someText);
+
+
+
+//arithmetic and assigment operators
+
+//var number1 = 7;
+//var number2 = 4;
+
+//number1  +=  number2;
+//number1++
+
+//document.write (number1);
+
+//Conditions:
+
+// var food = "3";
+
+// if (food === "3") {
+
+
+// alert (" We love strings");
+
+// }
+
+//var number1 = 4 + 3*5;
+//the multiplicator * and division/ operator is executed first than addition  and subtraction.
+//the above var on the console will give a result of 19 because it will excute first the multiplication operator 3*5 + 4 = 19.
+
+
+// to avoid that put inside parentheses the operator you would to be excuted first.
+
+
+console.log("Hello");
+
+console.log(" you are !");
+
+/*document.getElementById("demo").innerHTML = "My name is Blue";
+
+ var general;
+
+ general = 6;
+
+ console.log(typeof general);
+
+ let a =[ 1, 2, 4,];
+
+a[0]
+
+Assignment operators console.log(2-1);
+
+let diff= 3-2;
+
+console.log("diff ->" +diff);
+
+diff++;
+
+console.log(diff);
+
+ Comparison  Operators */
+
+
+let s=9;
+let s2=8;
+
+if (s==s2){
+
+	console.log("Oh look we are so alike");
+}
+
+else {
+
+console.log("We dont look so alike")};
+
+
+
+if ("blue" != "Blue"){
+
+	console.log("We are not too blue");
+}
+else {
+
+	console.log( "We are both blue")};
+
+
+	if (32 >= 30){
+
+		console.log( "You are greater or equal");
+	}
+
+else {
+
+	console.log("We are not great");
+}
+
+
+// boolean operators
+
+
+
+
+
